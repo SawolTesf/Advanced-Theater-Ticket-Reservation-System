c773cb9032e3dc307c8e1542e83b3b5ea24d5067
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-11-01 21:31:04 -0500

    Implement Seat class with properties and methods

--- src/Seat.java
+++ src/Seat.java
@@ -1,3 +1,41 @@
 public class Seat {
-    
+    private int row;
+    private char seat;
+    private char ticketType;
+
+    public Seat() {
+        this.row = 0;
+        this.seat = ' ';
+        this.ticketType = ' ';
+    }
+
+    public Seat(int row, char seat, char ticketType) {
+        this.row = row;
+        this.seat = seat;
+        this.ticketType = ticketType;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public char getSeat() {
+        return this.seat;
+    }
+
+    public void setSeat(char seat) {
+        this.seat = seat;
+    }
+
+    public char getTicketType() {
+        return this.ticketType;
+    }
+
+    public void setTicketType(char ticketType) {
+        this.ticketType = ticketType;
+    }
 }
