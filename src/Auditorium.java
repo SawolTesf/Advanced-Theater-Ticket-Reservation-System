111d65528696df9b486a2495b3e16b10a6880876
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-11-05 16:52:53 -0600

    Add Auditorium class with getFirst and setFirst methods

--- src/Auditorium.java
+++ src/Auditorium.java
@@ -1,3 +1,16 @@
-public class Auditorium {
-    
+public class Auditorium<T> {
+    private Node<T> first;
+
+    public Auditorium() {
+        this.first = null;
+    }
+
+    public Node<T> getFirst() {
+        return first;
+    }
+
+    public void setFirst(Node<T> first) {
+        this.first = first;
+    }
+
 }
