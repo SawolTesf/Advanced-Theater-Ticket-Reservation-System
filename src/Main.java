cd699617b159d2dc3a6c5dcb3216aeba6539d2d1
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-11-04 22:26:19 -0500

    Add main method with menu display and file input handling

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,34 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
+    public static void menu(){
+        System.out.println("1. Reserve Seats");
+        System.out.println("2. Exit");
+    }
+
+    // This is the main method that will be executed
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask user for file name and creste file object
+        System.out.print("Enter file name: ");
+        try{
+            String fileName = scanner.nextLine();
+            FileInputStream inFile = new FileInputStream(new File(fileName));
+
+            // Create auditorium object
+            Auditorium<Seat> auditorium = new Auditorium<>();
+
+            // Create scanner object to read from file
+            Scanner fileScanner = new Scanner(inFile);
+
+            scanner.close();
+            fileScanner.close();
+        }
+        catch(Exception e){
+            System.out.println("File not found");
+            System.exit(0);
+        }
     }
 }
